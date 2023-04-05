9189f8b9db9d0e8174d66712a6f4e3960f447316
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-04-05 15:43:10 +0700

    update

---
+++ src/main/java/com/demo/customerrestful/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.demo.customerrestful.repository;
+
+import com.demo.customerrestful.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/demo/customerrestful/service/ICustomerService.java
@@ -0,0 +1,9 @@
+package com.demo.customerrestful.service;
+
+import com.demo.customerrestful.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer>
+{
+
+
+}
